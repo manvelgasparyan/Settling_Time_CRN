a9f02c2f1feaef80170336674e3cd6828aed1ac3
Manvel Gasparyan <manvelgasparyan@gmail.com>
2021-01-05 14:49:29 +0900

    Create Model.m

---
+++ src/Model.m
@@ -0,0 +1,26 @@
+function [dx_dt] = Model (~, x)
+       %---------------------------
+       %Substrate composition matrix
+       Delta  = - min(S,0);
+       %---------------------------
+       %Diagonal of rate constants
+       Gamma_k = diag(k);
+       %---------------------------
+       %Substrate expression function
+       phi = exp(Delta'*log(x));
+       %---------------------------
+       %Rational terms in reaction rates
+       g = [1/(1 + x(1)/K(1) + x(2)/K(2) + x(3)/K(3));
+            1/(1 + x(1)/K(1) + x(2)/K(2) + x(3)/K(3));
+            1/(1 + (x(3)*x(4))/(K(4)*K(5)) + x(3)/K(4) + x(4)/K(5));
+            1/(1 + (x(3)*x(4))/(K(4)*K(5)) + x(3)/K(4) + x(4)/K(5))];
+       %---------------------------
+       %Diagonal of rational terms
+       Gamma_g = diag(g);
+       %---------------------------
+       %Reaction rates
+       v = Gamma_k*Gamma_g*phi;
+       %---------------------------
+       %The balance laws
+       dx_dt = S*v;
+    end
