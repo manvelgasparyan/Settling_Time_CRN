c14ea0f12b872fdb6f78b26f94e57f8651ee2c6e
Manvel Gasparyan <manvelgasparyan@gmail.com>
2021-01-05 15:02:06 +0900

    Create mathematical_model.m

---
+++ example/mathematical_model.m
@@ -0,0 +1,23 @@
+function [dx_dt] = mathematical_model (~, k, K, x)
+       %---------------------------
+       %Substrate composition matrix
+       Delta  = - min(S,0);
+       %---------------------------
+       %Diagonal of rate constants
+       Gamma_k = diag(k);
+       %---------------------------
+       %Substrate expression function
+       phi = exp(Delta'*log(x));
+       %---------------------------
+       %Rational terms in reaction rates
+       g = denominator(k,K,x);
+       %---------------------------
+       %Diagonal of rational terms
+       Gamma_g = diag(g);
+       %---------------------------
+       %Reaction rates
+       v = Gamma_k*Gamma_g*phi;
+       %---------------------------
+       %The balance laws
+       dx_dt = S*v;
+    end
